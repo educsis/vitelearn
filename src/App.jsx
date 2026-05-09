4014caaac0eaa9553ca4957e6e1ed8b4ba0f5ff0
Edwin Orellana <edwinorellana@users.noreply.github.com>
2026-05-08 21:50:10 -0600

    Update App.jsx and add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -117,6 +118,7 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
+      <Footer />
     </>
   );
 }
